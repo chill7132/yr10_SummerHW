0bb8e1380fa5b31fc248aa3eb1e171a376807ed5
Callum <chill7132@sohamcollege.org.uk>
2014-06-19 16:11:13 +0100

    Update tables.py
    This is the first task of the summer homework

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,11 @@
-#paste code here
+def Timestable():
+    num = int(input("Which timestable would you like to see?>>>"))#Asks user for an input of a number
+    if num <= 12:#Checks if the number is less or equal to 12
+        for i in range(1,13):
+           print(num,'x',i,'=',num*i)#Prints all the timestable for the chosen number
+    else:
+        print("A number between 1 and 12 please")#Tells the user they need to pick a number between 1 and 12
+        Timestable()#Replays the function so that the user can enter a number between 1 and 12
+
+Timestable()#Starts the function when the code is run instead of typing in the function name to get it to run
+ here
